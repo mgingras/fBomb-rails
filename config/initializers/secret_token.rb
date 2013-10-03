a8c516bc823633f4d5c35b572120cf7b9cf52bd3
Martin Gingras <martin.p.gingras@gmail.com>
2013-10-03 09:26:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FBomb::Application.config.secret_key_base = 'bfae1b32ec7fea0fb5476fae368c194110eaaa9b8e7bbffb4324df388c043653cc5ec9944940a85f6fb2251c2405ab4796192b6cc9cc0f6db8b73b3baec9b34c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FBomb::Application.config.session_store :cookie_store, key: '_fBomb_session'
